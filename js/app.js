9fd5237671f53c4647307ea6fed1fe34b0407cc5
ShahriarRaktim <shahriarahmedraktim@gmail.com>
2021-09-02 16:47:54 +0600

    update

--- js/app.js
+++ js/app.js
@@ -39,6 +39,7 @@ const loadData = (data) => {
     // ------ condition of book that dose't exist ----- 
     if(`data.num_found === 0`){
         spiner('none');
+        
     }
 
     // ------ set book list ------ 
@@ -51,12 +52,12 @@ const loadData = (data) => {
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-        <img class="w-75 mx-auto" src="https://covers.openlibrary.org/b/id/${data.cover_i}-M.jpg" class="card-img-top" alt="...">
+        <img class="w-75 mx-auto" src="https://covers.openlibrary.org/b/id/${data.cover_i ? data.cover_i: 10909258}-M.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h3 class="card-title">${data.title}</h3>
-            <h6><span>Author:</span> ${data?.author_name?.toString()}</h6>
-            <h6><span>Publisher:</span> ${data?.publisher?.toString()}</h6>
-            <h6><span>First Publish:</span> ${data.first_publish_year}</h6>
+            <h6><span>Author:</span> ${data.author_name ? data.author_name : 'Not Found Author'}</h6>
+            <h6><span>Publisher:</span> ${data.publisher ? data.publisher : 'Not Found Publisher'}</h6>
+            <h6><span>First Publish:</span> ${data.first_publish_year ? data.first_publish_year: 'Not Found Date'}</h6>
         </div>
         </div>
         `
